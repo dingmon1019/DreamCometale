21478bc232d0f5fde627623da4b722052e6b242e
hanbin-park <hanbin1730@naver.com>
2022-08-19 11:27:36 +0900

    220819 11:26
    함정 메이킹

--- fallguys/Assets/ObstacleCoursePack/Scripts/Pendulum.cs
+++ fallguys/Assets/ObstacleCoursePack/Scripts/Pendulum.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pendulum : MonoBehaviour
 {
-	public float speed = 1.5f;
+	public float speed;
 	public float limit = 75f; //Limit in degrees of the movement
 	public bool randomStart = false; //If you want to modify the start position
 	private float random = 0;
@@ -19,7 +19,7 @@ public class Pendulum : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		float angle = limit * Mathf.Sin(Time.time + random * speed);
+		float angle = limit * Mathf.Sin(Time.time * speed);
 		transform.localRotation = Quaternion.Euler(0, 0, angle);
 	}
-}
+}																													
