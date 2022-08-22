bc926865c0c580584a6ed74480d588bdf147d9b0
hanbin-park <hanbin1730@naver.com>
2022-08-22 16:30:45 +0900

    220822 16:30
    점프대 스크립트 연구

--- fallguys/Assets/Scripts/JumpBase.cs
+++ fallguys/Assets/Scripts/JumpBase.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class JumpBase : MonoBehaviour
 {
+    public float force = 10f;
+    public float stunTime = 0.001f;
+	private Vector3 hitDir;
 
-    void OnTriggerEnter(Collider other)
-    {
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        if(other.tag == "Player")
-        {
-            rb.AddForce(Vector3.up * 20f);
-        }
+	void OnCollisionEnter(Collision collision) //콜라이더 충돌
+	{
+		foreach (ContactPoint contact in collision.contacts) // 배열 인덱스 끝까지 알아서 순환
+        //형태: foreach(데이터타입 변수명 in 배열,컬렉션등)
+        //contacts[포지션 벡터, 노말 벡터, 색 벡터]
+		{
+			Debug.DrawRay(contact.point, contact.normal, Color.white);//Debug.DrawRay(지점, 방향, 색)
+			if (collision.gameObject.tag == "Player")
+			{
+				hitDir = contact.normal;
+				collision.gameObject.GetComponent<CharacterControls>().HitPlayer(-hitDir * force, stunTime);
+				return;
+			}
+		}
     }
-
 }
